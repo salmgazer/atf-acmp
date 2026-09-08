8d7296f7da8df9fbb715bd1121699975aadb2b43
Salifu Mutaru <salifumutaru@gmail.com>
2026-09-08 16:46:15 +0000

    fix deployment

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -22,7 +22,7 @@ variable "github_org" {
 variable "github_repo" {
   description = "GitHub repository name"
   type        = string
-  default     = "acmp"
+  default     = "atf-acmp"
 }
 
 variable "db_password" {
