b5942abc6783f916a5ad93c9543cd42b919c1cfc
Salifu Mutaru <salifumutaru@gmail.com>
2026-09-09 08:24:24 +0000

    fix: add missing next-themes and uuid dependencies for web build

--- deploy/terraform/iam.tf
+++ deploy/terraform/iam.tf
@@ -89,7 +89,10 @@ resource "aws_iam_role" "github_actions" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:${var.github_org}/${var.github_repo}:ref:refs/heads/*",
+            "repo:${var.github_org}/${var.github_repo}:environment:*"
+          ]
         }
       }
     }]
